ea6a4b0bc7bb00080439117222b6c0493f6e6a1b
mukul0596 <icecoolmkl@gmail.com>
2021-01-14 22:16:12 +0530

    debugging

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import DiscussionSection from './Components/DiscussionSection/DiscussionSection'
 import './App.css';
 
 function App() {
+  console.log(process.env.REACT_APP_FIRESTORE_API_KEY, process.env.REACT_APP_FIRESTORE_PROJECT_ID);
   return (
     <div className="App">
       <h1>Q&A</h1><h1>Portal</h1>
